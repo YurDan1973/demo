62679051837c9232759186f90fc85dc445c9a8cb
YurDan1973 <jura.danilov.73@gmail.com>
2024-11-13 16:42:58 +0300

    Initial commit

--- src/main/java/com/example/demo/rest/MyController.java
+++ src/main/java/com/example/demo/rest/MyController.java
@@ -2,23 +2,22 @@ package com.example.demo.rest;
 
 import com.example.demo.common.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-    @Autowired
     private Teacher teacher;
 
-//    @Autowired
-//// Эта аннотация над конструктором говорит о том, что если
-//// нижеследующему MyController - ру понадобится объект, указанный в параметрах, т.е. Teacher teacher
-//// то Spring её создат сам и подтянет её
-//    public MyController(Teacher teacher) {
-//
-//        this.teacher = teacher;
-//    }
+    @Autowired
+// Эта аннотация над конструктором говорит о том, что если
+// нижеследующему MyController - ру понадобится объект, указанный в параметрах, т.е. Teacher teacher
+// то Spring её создат сам и подтянет её
+    public MyController( @Qualifier("laborTeacher") Teacher teacher) {
+        this.teacher = teacher;
+    }
 
 //    @Autowired
 //    public void setTeacher(Teacher teacher) {
