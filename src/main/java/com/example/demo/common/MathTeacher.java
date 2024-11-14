28c53ebf9fab7e793fa896ada3fd7b3f3aeec402
YurDan1973 <jura.danilov.73@gmail.com>
2024-11-14 18:34:44 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/demo/common/MathTeacher.java
+++ src/main/java/com/example/demo/common/MathTeacher.java
@@ -1,5 +1,7 @@
 package com.example.demo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
@@ -10,11 +12,24 @@ import org.springframework.stereotype.Component;
 // будет создать экземпляр нижеуказанного (именно под аннотацией) класса
 //@Lazy
 //@Scope("prototype")
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class MathTeacher implements Teacher {
     public MathTeacher() {
         System.out.println("Bean of class - " + this.getClass().getSimpleName());
     }
+//    @PostConstruct
+//    // Эта аннотация говорит о том, что этот метод вызывается после создания конструктора
+//    public void myFirstMethod() {
+//        System.out.println("myFirstMethod");
+//    }
+
+//    @PreDestroy
+//    // Эта аннотация говорит о том, что этот метод вызывается после того как мы завершаем
+//    // действие бина, т.е. когда мы останавливаем наше приложение
+//    public void myLastMethod() {
+//        System.out.println("myLastMethod");
+//    }
+//    // Этот метод отрабатывает только если мы используем pattern проектирования  Singleton
 
     @Override
     public String getHomeWork() {
--- src/main/java/com/example/demo/rest/MyController.java
+++ src/main/java/com/example/demo/rest/MyController.java
@@ -12,18 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     private Teacher teacher;
-    private Teacher secondTeacher;
+//    private Teacher secondTeacher;
 
     @Autowired
 // Эта аннотация над конструктором говорит о том, что если
 // нижеследующему MyController - ру понадобится объект, указанный в параметрах, т.е. Teacher teacher
 // то Spring её создат сам и подтянет её
-    public MyController(@Qualifier("mathTeacher") Teacher teacher,
-                        @Qualifier("mathTeacher") Teacher secondTeacher) {
+//    public MyController(@Qualifier("mathTeacher") Teacher teacher
+    public MyController(@Qualifier("englishTeacher") Teacher teacher
+// , @Qualifier("mathTeacher") Teacher secondTeacher
+    ) {
 //    public MyController(Teacher teacher) {
         System.out.println("Bean of class - " + this.getClass().getName());
         this.teacher = teacher;
-        this.secondTeacher = secondTeacher;
+//        this.secondTeacher = secondTeacher;
     }
 
 //    @Autowired
@@ -38,8 +40,8 @@ public class MyController {
         return teacher.getHomeWork();
     }
 
-    @GetMapping("/compteachers")
-    public String compareTeachers() {
-        return "Compareteachers - " + (teacher == secondTeacher);
-    }
+//    @GetMapping("/compteachers")
+//    public String compareTeachers() {
+//        return "Compareteachers - " + (teacher == secondTeacher);
+//    }
 }
